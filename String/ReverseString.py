3c29a5f60ad9813635e2d7fd1f6dd21652bb6b68
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-07-06 16:14:35 +0530

    [Rikesh]Refactor: reverse string code and add it into a function

--- String/ReverseString.py
+++ String/ReverseString.py
@@ -2,18 +2,28 @@
 @Author: Rikesh Chhetri
 @Date: 2021-07-05 
 @Last Modified by: Rikesh Chhetri
-@Last Modified time: 2021-07-05 9:03:30
+@Last Modified time: 2021-07-06 2:03:30
 @Title : Program Aim is to reverse the string.
 """
 from LogHandler import logger
 
-try:
-    string1 = input("Enter the text to reverse: ")
-    # using slice 
-    start = stop = None # setting start and  stop as none
-    step = -1           # -1 last index  of string
-    reverse_slice = slice(start, stop, step)
-    logger.info(string1[reverse_slice])
-  
-except Exception as e:
-    logger.error(e)
+def reverseString(string1):
+    """
+    Description:
+        This function is used for reverse a string.
+    Parameter:
+        It takes string1 as a parameter.
+       
+    """
+
+    try:
+       
+        reverse_string = string1[::-1]
+        logger.info(reverse_string)
+
+    except Exception as e:
+        logger.error(e)
+
+if __name__ == "__main__":
+     string1 = input("Enter the text to reverse: ")
+     reverseString(string1)

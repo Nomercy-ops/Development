a88f72effb3a240a18a827f057d4f6b541b94592
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-07-06 16:21:32 +0530

    [Rikesh]Refactor: calculate length of string  program and added code inside function

--- String/CalculateLength.py
+++ String/CalculateLength.py
@@ -2,13 +2,24 @@
 @Author: Rikesh Chhetri
 @Date: 2021-07-05 
 @Last Modified by: Rikesh Chhetri
-@Last Modified time: 2021-07-05 9:03:30
+@Last Modified time: 2021-07-06 3:03:30
 @Title : Program Aim is to calculate length of the string.
 """
 from LogHandler import logger
-try:
+def calculateLength(string1):
+    """
+    Description:
+        This function is used for calculating the length of a string.
+    Parameter:
+        It takes string1 as a parameter.
+       
+    """
+    try:
+        # printing the value of the string and the length of the string
+        print("String is ", string1, " and length of the string is", len(string1))
+    except Exception as e:
+        logger.error(e)
+
+if __name__ == "__main__":
     string1 = "rikeshchauhan"
-    # printing the value of the string and the length of the string
-    print("String is ", string1, " and length of the string is", len(string1))
-except Exception as e:
-    logger.error(e)
+    calculateLength(string1)
